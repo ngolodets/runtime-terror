682055c5c7d70144ffa12989b84a90e67d8be8cb
Guelito1 <mflorescacravens@gmail.com>
2019-07-25 15:39:04 -0700

    delete dead code

--- client/src/DrinkShow.jsx
+++ client/src/DrinkShow.jsx
@@ -1,28 +1,17 @@
-
 import React from 'react';
-
 const DrinkShow = (props) => {
-  const ingredients = props.drink.ingredients ? props.drink.ingredients: [];
+  const ingredients = props.drink.ingredients ? props.drink.ingredients : [];
   const ingredientItems = ingredients.map((ingredient, i) => {
     return(
     <p key={i}>
-      {ingredient. measure}
+      {ingredient.measure}
       {ingredient.ingredient}
     </p>
     )
   })
-
-  //{ingredientItems}
-  // function NumberList(props) {
-  //   const numbers = props.numbers;
-  //   const listItems = numbers.map((number) =>
-  //     <li key={number.toString()}>
-  //       {number}
-  //     </li>
-  //   );
   return (
     <div className='drink-show'>
-      <img src={props.drink.picture} alt=""/>
+      <img src={props.drink.picture} alt="cocktail"/>
       <h3>{props.drink.drinkName} is a {props.drink.category} served in a {props.drink.glassType}</h3>
       <h4>{props.drink.instructions}</h4>
       <p>The list of ingredients:
